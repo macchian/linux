c921a2fbd81948a75fd83173f0b1ee876eda5d1c
Mac <mac.chiang@intel.com>
2021-08-11 02:58:23 -0400

    [WIP] ASoC: intel: boards: add max98390 support
    4ch configuration, echo reference not confirmed yet
    
    Signed-off-by: Mac Chiang <mac.chiang@intel.com>
    
    Change-Id: I6d0076c114a610d6ce4f1f03b85ccff57ff36349

--- sound/soc/intel/boards/sof_maxim_common.c
+++ sound/soc/intel/boards/sof_maxim_common.c
@@ -16,6 +16,7 @@ const struct snd_soc_dapm_route max_98373_dapm_routes[] = {
 	/* speaker */
 	{ "Left Spk", NULL, "Left BE_OUT" },
 	{ "Right Spk", NULL, "Right BE_OUT" },
+
 };
 EXPORT_SYMBOL_NS(max_98373_dapm_routes, SND_SOC_INTEL_SOF_MAXIM_COMMON);
 
@@ -132,6 +133,91 @@ void max_98373_set_codec_conf(struct snd_soc_card *card)
 	card->num_configs = ARRAY_SIZE(max_98373_codec_conf);
 }
 EXPORT_SYMBOL_NS(max_98373_set_codec_conf, SND_SOC_INTEL_SOF_MAXIM_COMMON);
+
+/*
+ * Maxim MAX98390
+ */
+
+static struct snd_soc_codec_conf max_98390_codec_conf[] = {
+        {
+                .dlc = COMP_CODEC_CONF(MAX_98390_DEV0_NAME),
+                .name_prefix = "Woofer Right",
+        },
+        {
+                .dlc = COMP_CODEC_CONF(MAX_98390_DEV1_NAME),
+                .name_prefix = "Woofer Left",
+        },
+        {
+                .dlc = COMP_CODEC_CONF(MAX_98390_DEV2_NAME),
+                .name_prefix = "Tweeter Right",
+        },
+        {
+                .dlc = COMP_CODEC_CONF(MAX_98390_DEV3_NAME),
+                .name_prefix = "Tweeter Left",
+        },
+};
+
+struct snd_soc_dai_link_component max_98390_components[] = {
+        {  /* For Woofer Right */
+                .name = MAX_98390_DEV0_NAME,
+                .dai_name = MAX_98390_CODEC_DAI,
+        },
+        {  /* For Woofer Left */
+                .name = MAX_98390_DEV1_NAME,
+                .dai_name = MAX_98390_CODEC_DAI,
+        },
+        {  /* For Tweeter Right */
+                .name = MAX_98390_DEV2_NAME,
+                .dai_name = MAX_98390_CODEC_DAI,
+        },
+        {  /* For Tweeter Left */
+                .name = MAX_98390_DEV3_NAME,
+                .dai_name = MAX_98390_CODEC_DAI,
+        },
+
+};
+EXPORT_SYMBOL_NS(max_98390_components, SND_SOC_INTEL_SOF_MAXIM_COMMON);
+
+static int max_98390_hw_params(struct snd_pcm_substream *substream,
+                               struct snd_pcm_hw_params *params)
+{
+        struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(substream);
+        struct snd_soc_dai *codec_dai;
+        int j;
+
+        for_each_rtd_codec_dais(rtd, j, codec_dai) {
+                if (!strcmp(codec_dai->component->name, MAX_98390_DEV0_NAME)) {
+                        /* DEV0 tdm slot configuration */
+                        snd_soc_dai_set_tdm_slot(codec_dai, 0x01, 1, 4, 32);
+                }
+                if (!strcmp(codec_dai->component->name, MAX_98390_DEV1_NAME)) {
+                        /* DEV1 tdm slot configuration */
+                        snd_soc_dai_set_tdm_slot(codec_dai, 0x02, 2, 4, 32);
+                }
+                if (!strcmp(codec_dai->component->name, MAX_98390_DEV2_NAME)) {
+                        /* DEVi2 tdm slot configuration */
+                        snd_soc_dai_set_tdm_slot(codec_dai, 0x04, 4, 4, 32);
+                }
+                if (!strcmp(codec_dai->component->name, MAX_98390_DEV3_NAME)) {
+                        /* DEV3 tdm slot configuration */
+                        snd_soc_dai_set_tdm_slot(codec_dai, 0x08, 8, 4, 32);
+                }
+        }
+        return 0;
+}
+
+struct snd_soc_ops max_98390_ops = {
+        .hw_params = max_98390_hw_params,
+        .trigger = max_98373_trigger,
+};
+EXPORT_SYMBOL_NS(max_98390_ops, SND_SOC_INTEL_SOF_MAXIM_COMMON);
+
+void max_98390_set_codec_conf(struct snd_soc_card *card)
+{
+        card->codec_conf = max_98390_codec_conf;
+        card->num_configs = ARRAY_SIZE(max_98390_codec_conf);
+}
+EXPORT_SYMBOL_NS(max_98390_set_codec_conf, SND_SOC_INTEL_SOF_MAXIM_COMMON);
 
 /*
  * Maxim MAX98357A/MAX98360A
